cea41005aa9c062878a5c42947ecbd78ad8abb08
jbonilla45 <jorge.bonilla12@misena.edu.co>
2021-10-25 20:00:24 -0500

    update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
+import {ToastrModule} from "ngx-toastr";
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/producto.ts
@@ -0,0 +1,14 @@
+export class Producto {
+  _id?: number;
+  nombre: string;
+  categoria: string;
+  ubicacion: string;
+  precio: string;
+
+  constructor(nombre: string, categoria: string, ubicacion: string, precio: string) {
+    this.nombre = nombre;
+    this.categoria= categoria;
+    this.ubicacion= ubicacion;
+    this.precio=precio;
+  }
+}
